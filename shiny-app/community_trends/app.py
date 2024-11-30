5da751c7c8c5e2b09c26082742741aa8e02601cd
kattu1 <katherinetu@uchicago.edu>
2024-11-30 05:16:47 -0600

    Updated app3 + datasets

--- shiny-app/community_trends/app.py
+++ shiny-app/community_trends/app.py
@@ -11,8 +11,15 @@ app_ui = ui.page_fluid(
                     label='Choose an Efficiency Type',
                     choices=['Electricity','Gas','Greenhouse Gas'],
                     selected='Greenhouse Gas'), 
-    output_widget('efficiency_trend'),
-    ui.output_table("subsetted_data_table")
+    ui.input_checkbox("show", "Show Data"),
+    ui.row(
+        ui.column(6, 
+            output_widget('efficiency_trend')),
+        ui.column(6,  
+            ui.panel_conditional(
+                "input.show",
+                ui.output_table("subsetted_data_table")))
+    )
 )
 
 
@@ -31,11 +38,23 @@ def server(input, output, session):
         df = subsetted_data()
         df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
         df = df.reset_index(drop=True)
-        df = df.rename(columns={"year": "Year",
-                                "Greenhouse Gas": "GHG Efficiency (Metric Tons CO2e/1000 sq ft)",
-                                "Electricity": "Electricity Efficiency (kBtu/1000 sq ft)",
-                                "Gas": "Gas Usage (kBtu/1000 sq ft)",
-                                "community": "Community Name"})
+
+        selected_emission = input.efficiency_type()
+        rename_map = {
+            "year": "Year",
+            "Greenhouse Gas": "GHG Efficiency (Metric Tons CO2e/1000 sq ft)",
+            "Electricity": "Electricity Efficiency (kBtu/1000 sq ft)",
+            "Gas": "Gas Usage (kBtu/1000 sq ft)"
+        }
+
+        columns_to_keep = ["year", selected_emission]
+
+        df = df[columns_to_keep]
+        df = df.rename(columns={
+            "year": rename_map["year"],
+            selected_emission: rename_map[selected_emission]
+        })
+
         return df
     
     @render_altair
@@ -45,7 +64,7 @@ def server(input, output, session):
 
         chart_data = df.rename(columns={selected_emission: "value"})
 
-        chart = alt.Chart(chart_data).mark_line(point = True).encode(
+        chart = alt.Chart(chart_data).mark_line().encode(
             alt.X("year:O"),
             alt.Y("value:Q").axis(title=f"{selected_emission}")
             ).properties(width = 500, height = 300)
